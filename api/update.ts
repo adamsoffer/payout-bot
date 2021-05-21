6316afeb52da4ddd2b84007421c43542831f90f6
adamsoffer <adam@livepeer.org>
2021-05-20 20:07:16 -0400

    update ppp

--- api/update.ts
+++ api/update.ts
@@ -5,7 +5,7 @@ import Twitter from "twitter";
 
 const fetch = require("@vercel/fetch")();
 
-const pricePerPixel = 0.0000000000000006; // (600 wei)
+const pricePerPixel = 0.0000000000000012; // (600 wei)
 
 // the # of pixels in a minute of 240p30fps, 360p30fps, 480p30fps, 720p30fps transcoded renditions.
 // (width * height * framerate * seconds in a minute)
